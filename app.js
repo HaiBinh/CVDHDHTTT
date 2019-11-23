b7c38bcf70a21999ccec643fa5a133a6ae804449
hb <onghoan2009@gmail.com>
2019-11-23 08:42:03 +0700

    fix bug

--- app.js
+++ app.js
@@ -52,3 +52,15 @@ try {
 
 // Start server
 sails.lift(rc('sails'));
+
+var mongoose = require('mongoose');
+var env = process.env.NODE_ENV || 'devolopment';
+var config = require('./config/mongo')[env];
+
+module.exports = () => {
+  var envUrl = process.env[config.use_env_variable];
+  var localUrl = `mongodb://${config.host}/${config.database}`;
+  var mongoUrl = envUrl ? envUrl : localUrl;
+  return mongoose.connect(mongoUrl);
+};
+
